e2f5d0076ad8741c7ccb63ced1ed36336a6d44c9
Joanna M. Rives <joanna.rives@gmail.com>
2018-07-25 13:22:39 -0700

    ADD Initializer function.

--- baby_dragon.py
+++ baby_dragon.py
@@ -1,4 +1,14 @@
 #! /Library/Frameworks/Python.framework/Versions/3.7/bin/python3.7
 # -*- coding: utf-8 -*-
 class Dragon:
+    def __init__(self, name, snuggled, sleep, hunger, cloaca, fire, energy):
+        self.name = name
+        self.snuggled = snuggled
+        self.sleep = sleep
+        self.hunger = hunger
+        self.cloaca = cloaca
+        self.fire = fire
+        self.energy = energy
+
+class BabyDragon:
     pass
