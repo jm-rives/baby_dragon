7e9aa867288263db644458d754353bc1aae2b15e
Joanna M. Rives <joanna.rives@gmail.com>
2018-07-26 15:10:38 -0700

    Add empty time passess and snuggle instance attributes.

--- baby_dragon.py
+++ baby_dragon.py
@@ -1,8 +1,11 @@
 #! /Library/Frameworks/Python.framework/Versions/3.7/bin/python3.7
 # -*- coding: utf-8 -*-
+# Parent class
 class Dragon:
 
-    def __init__(self, name, snuggled, sleep, hunger, cloaca, fire, energy):
+    def __init__(self, name, snuggled,
+        sleep, hunger, cloaca,
+         fire, energy, maturity):
         self.name = name
         self.snuggled = snuggled
         self.sleep = sleep
@@ -10,14 +13,28 @@ class Dragon:
         self.cloaca = cloaca
         self.fire = fire
         self.energy = energy
+        self.maturity = maturity
 
-    def description(self, name, snuggled, sleep, hunger, cloaca, fire, energy):
+    # instance attribute
+    def time_passes():
         pass
         
-    def snuggle(self, snuggle):
-        snuggle_meter = 5
+    def description(self):
+        return """{} is snuggled at level {},
+         has {} units of sleep,
+         is {} level hungry,
+         needs to use the bath room on a scale of {} on a scale of 0 - 5,
+         has a fire power of {} on a scale of 0 - 5,
+         and has {} energy on a 0 - 5 scale.
+        """.format(self.name, self.snuggled, self.sleep,
+        self.hunger, self.cloaca,
+         self.fire, self.energy, self.maturity)
 
-        if snuggle_meter >= 3:
-            is_snuggled = True
-        else:
-            is_snuggled = False
+    def snuggle(self, snuggle):
+        pass
+
+# test code
+timmy = Dragon('Timmy', 5, 5, 0, 0, 0, 5, 1)
+
+print(timmy.name)
+print(timmy.description())
